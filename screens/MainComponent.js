493e222dc0c9dcfcbb4d1569c77350588a36b01e
Robert Dain <bobdain2@gmail.com>
2023-01-14 00:04:20 -0800

    nav part 1

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,26 +1,50 @@
-import { useState } from 'react';
-import { CAMPSITES } from '../shared/campsites.js';
+import { Platform, View } from 'react-native';
+import Constants from 'expo-constants';
+import { createStackNavigator } from '@react-navigation/stack';
 import DirectoryScreen from './DirectoryScreen';
-import { View } from 'react-native';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 
-const Main = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    const [selectedCampsiteId, setSelectedCampsiteId] = useState();
+const DirectoryNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator
+            initialRouteName='Directory'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#5637DD'
+                },
+                headerTintColor: '#fff'
+            }}>
 
+            <Stack.Screen
+                name='Directory'
+                component={DirectoryScreen}
+                options={{ title: 'Campsite Directory' }}>
+            </Stack.Screen>
+
+            <Stack.Screen
+                name='CampsiteInfo'
+                component={CampsiteInfoScreen}
+                options={({ route }) => ({
+                    title: route.params.campsite.name
+                })}>
+            </Stack.Screen>
+
+        </Stack.Navigator>
+    );
+}
+
+const Main = () => {
     return (
         // Flex: 1 makes it fill the space vertically
-        <View style={{ flex: 1 }}>
-            <DirectoryScreen
-                campsites={campsites}
-                onPress={campsiteId => setSelectedCampsiteId(campsiteId)}
-            />
+        <View style={{
+            flex: 1,
+            paddingTop:
+                Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            }}>
 
-            <CampsiteInfoScreen
-                campsite={
-                    campsites.filter(campsite => campsite.id === selectedCampsiteId)[0]
-                }
-            />
+            <DirectoryNavigator />
+
         </View>
     );
 }
