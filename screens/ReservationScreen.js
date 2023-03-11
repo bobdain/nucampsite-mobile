51fb37284de00123a26bb3ded505dfa6c42ce2e4
Robert Dain <bobdain2@gmail.com>
2023-03-10 19:16:57 -0600

    all weekly exercises

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -1,15 +1,23 @@
 import { useState } from 'react';
-import { Text, View, ScrollView, StyleSheet, Switch, Button, Alert } from 'react-native';
+import {
+    Text,
+    View,
+    ScrollView,
+    StyleSheet,
+    Switch,
+    Button,
+    Alert
+} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
     const [hikeIn, setHikeIn] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
-    //const [showModal, setShowModal] = useState(false);
 
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
@@ -17,28 +25,77 @@ const ReservationScreen = () => {
         setDate(currentDate);
     };
 
-    // const handleReservation = () => {
-    //     console.log('campers:', campers);
-    //     console.log('hikeIn:', hikeIn);
-    //     console.log('date:', date);
-
-    //     //setShowModal(!showModal);
-    // };
+    const handleReservation = () => {
+        const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString('en-US')}`;
+        Alert.alert(
+            'Begin Search?',
+            message,
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        resetForm();
+                    },
+                    style: 'cancel'
+                },
+                {
+                    text: 'OK',
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
+                }
+            ],
+            { cancelable: false }
+        );
+        console.log('campers:', campers);
+        console.log('hikeIn:', hikeIn);
+        console.log('date:', date);
+    };
 
     const resetForm = () => {
         setCampers(1);
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
-    }
+    };
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    };
 
     return (
         <ScrollView>
-            <Animatable.View
-                animation='zoomIn'
-                duration={2000}
-                delay={1000}
-            >
+            <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers:</Text>
                     <Picker
@@ -69,7 +126,7 @@ const ReservationScreen = () => {
                         onPress={() => setShowCalendar(!showCalendar)}
                         title={date.toLocaleDateString('en-US')}
                         color='#5637DD'
-                        accessibilityLabel='Tap to select a reservation date'
+                        accessibilityLabel='Tap me to select a reservation date'
                     />
                 </View>
                 {showCalendar && (
@@ -83,67 +140,12 @@ const ReservationScreen = () => {
                 )}
                 <View style={styles.formRow}>
                     <Button
-                        onPress={() => {
-                            Alert.alert(
-                                'Begin Search?',
-                                `Number of campers: ${campers}\n\n` +
-                                `Hike-In: ${hikeIn}\n\n` +
-                                `Date: ${date.toLocaleDateString('en-US')}`,
-                                [
-                                    {
-                                        text: 'Cancel',
-                                        onPress: () =>
-                                            console.log('hi bob'),
-                                        style: 'cancel'
-                                    },
-                                    {
-                                        text: 'OK',
-                                        onPress: () =>
-                                        console.log('hi bob')
-                                    }
-                                ],
-                                { cancelable: false }
-                            )
-                        }}
+                        onPress={() => handleReservation()}
                         title='Search Availability'
                         color='#5637DD'
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-
-                {/* <Modal
-                    animationType='slide'
-                    transparent={false}
-                    visible={showModal}
-                    onRequestClose={() => setShowModal(!showModal)}
-                >
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>
-                            Search Campsite Reservations
-                        </Text>
-
-                        <Text style={styles.modalText}>
-                            Number of Campers: {campers}
-                        </Text>
-
-                        <Text style={styles.modalText}>
-                            Hike-In?: {hikeIn ? 'Yes' : 'No'}
-                        </Text>
-
-                        <Text style={styles.modalText}>
-                            Date: {date.toLocaleDateString('en-US')}
-                        </Text>
-
-                        <Button
-                            onPress={() => {
-                                setShowModal(!showModal);
-                                resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close'
-                        />
-                    </View>
-                </Modal> */}
             </Animatable.View>
         </ScrollView>
     );
@@ -163,23 +165,7 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
-    },
-    modal: {
-        justifyContent: 'center',
-        margin: 20
-    },
-    // modalTitle: {
-    //     fontSize: 24,
-    //     fontWeight: 'bold',
-    //     backgroundColor: '#5637DD',
-    //     textAlign: 'center',
-    //     color: '#fff',
-    //     marginBottom: 20
-    // },
-    // modalText: {
-    //     fontSize: 18,
-    //     margin: 10
-    // }
+    }
 });
 
 export default ReservationScreen;
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -2,4 +2,4 @@
 // Then find a block that has the Default Gateway populated
 // Copy the IPv4 address here
 // Make sure it ends with ":3001/"
-export const baseUrl = 'http://192.168.254.50:3001/';
+export const baseUrl = 'http://192.168.1.250:3001/';
